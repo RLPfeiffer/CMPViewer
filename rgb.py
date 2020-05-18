473e977db52e7c6f186c5d63dd837a16cba72ebb
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-05-18 12:39:14 -0600

    Use numpy arrays for compositing

--- rgb.py
+++ rgb.py
@@ -1,27 +1,32 @@
 from PyQt5.QtGui import QImage
+from PyQt5.QtGui import QColor
 import code
+from qimage2ndarray import *
+import time
 
-r_image = None
-g_image = None
-b_image = None
+def create_composite_image(r_image, g_image, b_image):    
+    # Get numpy representation of images for optimized color manipulation
+    r_optimized = rgb_view(r_image)
+    g_optimized = rgb_view(g_image)
+    b_optimized = rgb_view(b_image)
 
-def create_composite_image(r_image, g_image, b_image):
-    rgb_image = r_image.copy()
-    if g_image = None:
-        
-    print('rgb_image' in locals())
+    # Make an ndarray for the composite
+    composite_optimized = r_optimized.copy()
+
+    # Assign the composite pixels
     for x in range(r_image.width()):
         for y in range(r_image.height()):
-              # We need to calculate this pixel's composite color
-              r = r_image.pixelColor(x, y).value()
-              g = g_image.pixelColor(x, y).value()
-              b = b_image.pixelColor(x, y).value()
-              new_color = QColor(r, g, b)
-              rgb_image.setPixelColor(x, y, new_color)
+            # We need to calculate this pixel's composite color
+            r = r_optimized[y, x, 0]
+            g = g_optimized[y, x, 0]
+            b = b_optimized[y, x, 0]
 
-        return rgb_image
-
-    code.interact(local=locals())
+            composite_optimized[y, x, 0] = r
+            composite_optimized[y, x, 1] = g
+            composite_optimized[y, x, 2] = b
+    
+    # Convert our composite back to QImage
+    return array2qimage(composite_optimized)
 
 # def chooseRedImage(rImage):
 #     r_image = rImage
@@ -43,35 +48,17 @@ def create_composite_image(r_image, g_image, b_image):
 
 
 if __name__ == "__main__":
-    r_image = QImage("test_images/7564_12191_RLP_40x_05_TT.tif.tif")
-    g_image = QImage("test_images/7564_12191_RLP_40x_04_Q.tif.tif")
-    b_image = QImage("test_images/7564_12191_RLP_40x_06_E.tif.tif")
+    # Load images and convert them to numpy-compatible pixel format.
+    r_image = QImage("test_images/7564_12191_RLP_40x_05_TT.tif.tif").convertToFormat(QImage.Format_RGB32)
+    g_image = QImage("test_images/7564_12191_RLP_40x_04_Q.tif.tif").convertToFormat(QImage.Format_RGB32)
+    b_image = QImage("test_images/7564_12191_RLP_40x_06_E.tif.tif").convertToFormat(QImage.Format_RGB32)
     #chooseRedImage(rImage)
-    create_composite_image(r_image, g_image, b_image)
+    print("Compositing started")
+    time_start = time.perf_counter_ns()
+    our_composite = create_composite_image(r_image, g_image, b_image)
+    time_end = time.perf_counter_ns()
+    print('It took {} nanoseconds to composite'.format(time_end - time_start))
+
     #chooseGreenImage(gImage)
     #chooseBlueImage(bImage)
-    #code.interact(local=locals())
-
-# def get_image(r):
-
-
-#def create_composite_image():
-
-#   r_image = get_image(r)
-#   g_image = get_image(g)
-#   b_image = get_image(b)
-#
-#   our_composite_image = r_image.copy()
-#
-#   We assume that every image has the same dimensions! So it doesn't
-#   matter which one we loop:
-#   for x in range(r_image.width()):
-#       for y in range(r_image.height()):
-#           # We need to calculate this pixel's composite color
-#           r = r_image.pixelColor(x, y).value()
-#           g = g_image.pixelColor(x, y).value()
-#           b = b_image.pixelColor(x, y).value()
-#           new_color = QColor(r, g, b)
-#           our_composite_image.setPixelColor(x, y, new_color)
-#
-#   return our_composite_image
+    code.interact(local=locals())
