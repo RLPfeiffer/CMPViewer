f39c48d2bac6966e4065ef357f385b2449dfc36d
RLPfeiffer <r.pfeiffer@utah.edu>
2023-02-02 08:41:00 -0700

    Refactored image import to numpy/opencv to facilitate future clustering.

--- ImageViewer.py
+++ ImageViewer.py
@@ -42,19 +42,19 @@ class ImageViewerUi(QMainWindow):
     b_image = None
 
     """View Gui"""
+
     def __init__(self):
         """View Initializer"""
         super().__init__()
-        #Some main properties of the Window
+        # Some main properties of the Window
         self.setWindowTitle('ImageViewer')
 
-        #Set central widget and the general layout
+        # Set central widget and the general layout
         self.generalLayout = QHBoxLayout()
         self._centralWidget = QScrollArea()
         self.setCentralWidget(self._centralWidget)
         self._centralWidget.setLayout(self.generalLayout)
         self._centralWidget.setMinimumSize(1500, 800)
-
 
         """Don't forget to actually create a display"""
         self._createDisplay()
@@ -87,10 +87,10 @@ class ImageViewerUi(QMainWindow):
         clusterMenu.addAction(selectImagesAct)
 
     def _createViewList(self):
-        #create file view list
+        # create file view list
         self.ViewList_Box = QtWidgets.QGroupBox('Open Images')
         self.ViewList_Box.setMinimumSize(400, 200)
-        self.ViewList_Layout  = QtWidgets.QVBoxLayout()
+        self.ViewList_Layout = QtWidgets.QVBoxLayout()
         self.ViewList_Box.setLayout(self.ViewList_Layout)
 
         self.ViewList_Layout.setSpacing(2)
@@ -99,37 +99,41 @@ class ImageViewerUi(QMainWindow):
         self.generalLayout.addWidget(self.ViewList_Box)
 
     def chooseGrayscaleImage(self, index):
-        pixmap = QPixmap.fromImage(self.rawImages[index])
-        self.displayImage.setPixmap((pixmap).scaled(2000,5000, Qt.KeepAspectRatio))
+        img_array = self.rawImages[index]
+
+        gray1D = img_array.tobytes()
+        qImg = QImage(gray1D, img_array.shape[1], img_array.shape[0], QImage.Format_Indexed8)
+        pixmap = QPixmap.fromImage(qImg)
+        self.displayImage.setPixmap(pixmap)
         self.adjustSize()
 
     def chooseRedImage(self, index):
         self.r_image = self.rawImages[index]
-        composite = create_composite_image(self.r_image, self.g_image, self.b_image)
+        composite = create_composite_image(self.rawImages, self.r_image, self.g_image, self.b_image)
         pixmap = QPixmap.fromImage(composite)
-        self.displayImage.setPixmap((pixmap).scaled(2000,5000, Qt.KeepAspectRatio))
+        self.displayImage.setPixmap((pixmap).scaled(2000, 5000, Qt.KeepAspectRatio))
         self.adjustSize()
 
     def chooseGreenImage(self, index):
         self.g_image = self.rawImages[index]
-        composite = create_composite_image(self.r_image, self.g_image, self.b_image)
+        composite = create_composite_image(self.rawImages, self.r_image, self.g_image, self.b_image)
         pixmap = QPixmap.fromImage(composite)
-        self.displayImage.setPixmap((pixmap).scaled(2000,5000, Qt.KeepAspectRatio))
+        self.displayImage.setPixmap((pixmap).scaled(2000, 5000, Qt.KeepAspectRatio))
         self.adjustSize()
 
     def chooseBlueImage(self, index):
         self.b_image = self.rawImages[index]
-        composite = create_composite_image(self.r_image, self.g_image, self.b_image)
+        composite = create_composite_image(self.rawImages, self.r_image, self.g_image, self.b_image)
         pixmap = QPixmap.fromImage(composite)
-        self.displayImage.setPixmap((pixmap).scaled(2000,5000, Qt.KeepAspectRatio))
+        self.displayImage.setPixmap((pixmap).scaled(2000, 5000, Qt.KeepAspectRatio))
         self.adjustSize()
 
     def _createDisplay(self):
-        #Create display widget
+        # Create display widget
         self.display = QScrollArea()
         self.displayImage = QLabel()
 
-        #Set up display window properties
+        # Set up display window properties
         self.display.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.display.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.display.setWidgetResizable(True)
@@ -138,7 +142,7 @@ class ImageViewerUi(QMainWindow):
         self.display.setAlignment(Qt.AlignCenter)
         self.display.setMinimumSize(600, 1000)
 
-        #add this display to the general layout
+        # add this display to the general layout
         self.generalLayout.addWidget(self.display)
 
     def openImages(self):
@@ -163,63 +167,66 @@ class ImageViewerUi(QMainWindow):
         Imports images into UI
         :param str fileName: Filename designated by openImages
         :return: viewable image with color select radio buttons
-        :rtype: QImage
+        :rtype: numpy nd array
         '''
-        self.rawImages.append(QImage(fileName).convertToFormat(QImage.Format_RGB32))
+        image = cv2.imread(fileName, cv2.IMREAD_GRAYSCALE)
+        self.rawImages.append(image)
+
     def colorRBs(self, fileName, index):
         row = QtWidgets.QGroupBox()
         rowLayout = QtWidgets.QHBoxLayout()
-    
-        #Add Filenames associated with RBs
+
+        # Add Filenames associated with RBs
         basefileName = os.path.basename(fileName)
         simpleName = os.path.splitext(basefileName)[0]
         rowLayout.addWidget(QLabel(simpleName))
 
-         #Adding buttons for grayscale
+        # Adding buttons for grayscale
         grayRadioButton = QRadioButton('gray')
-        grayRadioButton.toggled.connect(lambda:self.chooseGrayscaleImage(index))
+        grayRadioButton.toggled.connect(lambda: self.chooseGrayscaleImage(index))
         rowLayout.addWidget(grayRadioButton)
         self.column1.addButton(grayRadioButton)
 
-    #Adding buttons for red
+        # Adding buttons for red
         redRadioButton = QRadioButton("R")
-        redRadioButton.toggled.connect(lambda:self.chooseRedImage(index))
+        redRadioButton.toggled.connect(lambda: self.chooseRedImage(index))
         rowLayout.addWidget(redRadioButton)
         self.redRBlist.addButton(redRadioButton)
 
-    #Adding buttons for green
+        # Adding buttons for green
         greenRadioButton = QRadioButton("G")
-        greenRadioButton.toggled.connect(lambda:self.chooseGreenImage(index))
+        greenRadioButton.toggled.connect(lambda: self.chooseGreenImage(index))
         rowLayout.addWidget(greenRadioButton)
         self.greenRBlist.addButton(greenRadioButton)
-    #Adding buttons for blue
+        # Adding buttons for blue
         blueRadioButton = QRadioButton("B")
-        blueRadioButton.toggled.connect(lambda:self.chooseBlueImage(index))
+        blueRadioButton.toggled.connect(lambda: self.chooseBlueImage(index))
         rowLayout.addWidget(blueRadioButton)
         self.blueRBlist.addButton(blueRadioButton)
 
         row.setLayout(rowLayout)
         self.ImportLayout.addWidget(row)
 
-     #Select images for clustering using GUI 
+    # Select images for clustering using GUI
     def selectClustImages(self):
         '''
         Select images for clustering using GUI
         '''
         self.clusterview = clusterSelect(self.fileNameList)
         self.clusterview.show()
-    
 
-#Client code
+
+# Client code
 def main():
     """Main function"""
-    #Create QApplication instance (remember 1 per project)
+    # Create QApplication instance (remember 1 per project)
     CMPViewer = QApplication(sys.argv)
-    #show the UI
+    # show the UI
     view = ImageViewerUi()
     view.show()
-    #Execute the main loop
+    # Execute the main loop
     sys.exit(CMPViewer.exec_())
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
--- rgb.py
+++ rgb.py
@@ -4,37 +4,38 @@ import code
 from qimage2ndarray import *
 import time
 import cv2
+import numpy as np
 
-def create_composite_image(r_image, g_image, b_image):
+def create_composite_image(rawImages, r_image, g_image, b_image):
     # Get numpy representation of images for optimized color manipulation
-    r_image = r_image or g_image or b_image
-    g_image = g_image or r_image or b_image
-    b_image = b_image or r_image or g_image
+    shape = rawImages[0].shape[:2]
 
-    r_optimized = rgb_view(r_image)
-    g_optimized = rgb_view(g_image)
-    b_optimized = rgb_view(b_image)
+    r = r_image
+    g = g_image
+    b = b_image
 
-    # Make an ndarray for the composite
-    r,_,_ = cv2.split(r_optimized)
-    g,_,_ = cv2.split(g_optimized)
-    b,_,_ = cv2.split(b_optimized)
+    if r is None:
+        r = np.zeros(shape=shape, dtype=np.uint8)
+    if g is None:
+        g = np.zeros(shape=shape, dtype=np.uint8)
+    if b is None:
+        b = np.zeros(shape=shape, dtype=np.uint8)
 
     # Convert our composite back to QImage
-    return array2qimage(cv2.merge((r, g, b)))
+    return array2qimage(np.dstack((r, g, b)))
 
-if __name__ == "__main__":
-    # Load images and convert them to numpy-compatible pixel format.
-    r_image = QImage("test_images/7564_12191_RLP_40x_05_TT.tif.tif").convertToFormat(QImage.Format_RGB32)
-    g_image = QImage("test_images/7564_12191_RLP_40x_04_Q.tif.tif").convertToFormat(QImage.Format_RGB32)
-    b_image = QImage("test_images/7564_12191_RLP_40x_06_E.tif.tif").convertToFormat(QImage.Format_RGB32)
-    #chooseRedImage(rImage)
-    print("Compositing started")
-    time_start = time.perf_counter_ns()
-    our_composite = create_composite_image(r_image, g_image, b_image)
-    time_end = time.perf_counter_ns()
-    print('It took {} nanoseconds to composite'.format(time_end - time_start))
-
-    #chooseGreenImage(gImage)
-    #chooseBlueImage(bImage)
-    code.interact(local=locals())
+# if __name__ == "__main__":
+#     # Load images and convert them to numpy-compatible pixel format.
+#     r_image = QImage("test_images/7564_12191_RLP_40x_05_TT.tif.tif").convertToFormat(QImage.Format_RGB32)
+#     g_image = QImage("test_images/7564_12191_RLP_40x_04_Q.tif.tif").convertToFormat(QImage.Format_RGB32)
+#     b_image = QImage("test_images/7564_12191_RLP_40x_06_E.tif.tif").convertToFormat(QImage.Format_RGB32)
+#     #chooseRedImage(rImage)
+#     print("Compositing started")
+#     # time_start = time.perf_counter_ns()
+#     our_composite = create_composite_image(r_image, g_image, b_image)
+#     time_end = time.perf_counter_ns()
+#     print('It took {} nanoseconds to composite'.format(time_end - time_start))
+#
+#     #chooseGreenImage(gImage)
+#     #chooseBlueImage(bImage)
+#     code.interact(local=locals())
